3bec0545940e1ed83c5628e6d35d0f3b9fa10853
Abhishek <s.abhishek7070@gmail.com>
2025-03-03 11:27:01 +0530

    modified

--- src/components/WhatsApp.jsx
+++ src/components/WhatsApp.jsx
@@ -14,10 +14,10 @@ const WhatsApp = () => {
             href={whatsappLink}
             target="_blank"
             rel="noopener noreferrer"
-            className="fixed bottom-6 right-6 flex items-center gap-2 bg-green-500  hover:bg-green-600 shadow-lg  transition duration-300 animate-bounce px-2 py-1 rounded-full"
+            className="fixed bottom-6 right-6 flex items-center md:gap-2 gap-1 bg-green-500  hover:bg-green-600 shadow-lg transition duration-300 animate-bounce md:px-2 md:py-1 rounded-full"
         >
 
-            <div className="bg-white relative flex items-center justify-center w-16 h-16 rounded-full overflow-hidden">
+            <div className="bg-white md:block md:w-16 md:h-16 w-10 h-10 hidden rounded-full overflow-hidden">
                 <img className="object-cover" src={profile} alt="" />
             </div>
 
@@ -27,7 +27,7 @@ const WhatsApp = () => {
                 <img
                     src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg"
                     alt="WhatsApp"
-                    className="w-10 h-10"
+                    className="md:w-10 md:h-10 w-8 h-8"
                 />
             </div>
         </a>
